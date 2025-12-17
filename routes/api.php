f1a30c9aa92b70aeaed9cb4db628954f3b7045f6
Danu Candra Saputra <danucahndx33@gmail.com>
2025-12-17 18:04:46 +0700

    tess

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\MidtransCallbackController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::post('/midtrans/callback', [MidtransCallbackController::class, 'handle']);
