30497aa9409b24c2275cae2f0f5b89e2f73703d7
Danu Candra Saputra <danucahndx33@gmail.com>
2025-12-17 14:28:56 +0700

    testing branch danu

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -10,14 +10,20 @@ class UsersSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->count(15)->create();
-
-        // Optional admin
+        // Admin user
         User::create([
             'name' => 'Admin Super',
-            'email' => 'admin@example.com',
+            'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
             'role' => 'admin',
+        ]);
+
+        // Customer user
+        User::create([
+            'name' => 'Customer User',
+            'email' => 'customer@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'customer',
         ]);
     }
 }
